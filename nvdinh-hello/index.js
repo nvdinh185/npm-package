2b3e0b0495e16435863af9036b189de751cc9c67
nvdinh185 <nvdinh0766777123@gmail.com>
2020-09-25 17:03:54 +0700

    edit

--- nvdinh-hello/index.js
+++ nvdinh-hello/index.js
@@ -1,6 +1,1 @@
-module.exports.area = radius => Math.PI * radius * radius
-module.exports.perimeter = radius => 2 * Math.PI * radius
-module.exports.sum = (a, b) => a + b
-module.exports.sub = (a, b) => a - b
-module.exports.mul = (a, b) => a * b
-module.exports.div = (a, b) => a / b
+module.exports = require("./sum")
---
+++ project/test2.js
@@ -0,0 +1,3 @@
+const tong = require("../nvdinh-hello/tong")
+
+console.log(tong(1, 2));
